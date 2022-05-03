5ceaa282bd867efe87204457463b109ec26e8665
FernandoGR <gamboa_029@hotmail.com>
2022-05-03 18:40:33 -0500

    W3E5V2

---
+++ app.js
@@ -0,0 +1,25 @@
+//Usando objeto express
+const express = require('express')
+
+//app de Express
+const app = express()
+app.use(express.json()) //Indicamos que se usara json
+
+//puerto en que vamos a ver nuestra app: localhost:3000
+const port = 3000
+
+//Con esto inicializamos esta app
+app.listen(port, () =>{
+    console.log(`Example aoo listening on port ${port}`);
+})
+
+//HTTP METHODS
+app.get('/v1/explorers', (req, res) =>{
+    console.log(`API Explorers GET ALL requests ${new Date()}`)
+    const explorer1 = {id:1, name:"Gambi1"}
+    const explorer2 = {id:2, name:"Gambi2"}
+    const explorer3 = {id:3, name:"Gambi3"}
+    const explorer4 = {id:4, name:"Gambi4"}
+    const explorers = [explorer1, explorer2, explorer3, explorer4]
+    res.status(200).json(explorers)
+})
